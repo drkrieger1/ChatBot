aea891148cccff222e1eee1f2e2a53c9ea164385
drkrieger1 <eplyushko@gmail.com>
2018-08-04 14:58:04 -0700

    basic bot created

--- ChatBot/Startup.cs
+++ ChatBot/Startup.cs
@@ -2,30 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChatBot.Bot;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Bot.Builder.BotFramework; 
+//using Microsoft.Extensions.Logging;
+//using Microsoft.Extensions.Options;
 
 namespace ChatBot
 {
     public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+    {  
 
-        public IConfiguration Configuration { get; }
+        //Inject IHostingEnviroment into constructor
+        public Startup(IHostingEnvironment env)
+        {
+            ContentRootPath = env.ContentRootPath;
+        }
+        //Track the Root path so that it can be used to set up configuration
+        public string ContentRootPath { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //Set up service configuration
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            var configuration = builder.Build();
+            services.AddSingleton(configuration);
+
+            //Add Echo Bot to the application
+            services.AddBot<EchoBot>(options =>
+            {
+                options.CredentialProvider = new ConfigurationCredentialProvider(configuration);
+            });
+
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,9 +64,10 @@ namespace ChatBot
             {
                 app.UseHsts();
             }
-
+            app.UseStaticFiles();
+            app.UseBotFramework();
             app.UseHttpsRedirection();
-            app.UseMvc();
+            //app.UseMvc();
         }
     }
 }
