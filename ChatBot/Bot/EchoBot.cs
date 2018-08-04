a27c57c6b4c5ff2e99602be008bfd75bc951b70b
drkrieger1 <eplyushko@gmail.com>
2018-08-04 13:28:48 -0700

    Project init

---
+++ ChatBot/Bot/EchoBot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+
+namespace ChatBot.Bot
+{
+    public class EchoBot : IBot
+    {
+        public Task OnTurn(ITurnContext turnContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
